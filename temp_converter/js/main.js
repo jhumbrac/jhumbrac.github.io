2f6f1ffb8f6d83fc5811fc3431aaa3bbc2b1c1b8
jhumbrac <jhumbrac@gmail.com>
2015-05-12 10:57:54 -0500

    temp update

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -3,32 +3,25 @@ function celConvert() {
     var degFloat = parseFloat(degC);
     var degF = degFloat*(9/5)+32;
 
-    $(".display").html(degFloat.toFixed() + "&#176;C" + " = " + degF.toFixed() + "&#176;F");
+    if(isNaN(degFloat)){
+        $(".display").html("Please enter a number");
+        $("p").addClass("error");
+    } else {
+        $(".display").html(degFloat.toFixed() + "&#176;C" + " = " + degF.toFixed() + "&#176;F");
+        $("p").removeClass("error");
+    }
     
     if(degF >= 120){
-        $("body").css("background-image", "url(" + imgUrl[3] + ")");
-        $("div").css("background-color", "#F44336");
-        $("div.answer").css("background-color", "#FF5252");
-        $("button").css("background-color", "#FF5252");
-        
+        refreshUI(imgUrl[3], "#F44336", "#FF5252");
     } else if(degF > 80 && degF < 120){
-        $("body").css("background-image", "url(" + imgUrl[2] + ")");
-        $("div").css("background-color", "#FF9800");
-        $("div.answer").css("background-color", "#FFAB40");
-        $("button").css("background-color", "#FFAB40");
-        
-    } else if(degF <= 80 && degF >= 50){
-        $("body").css("background-image", "url(" + imgUrl[1] + ")");
-        $("div").css("background-color", "#4CAF50");
-        $("div.answer").css("background-color", "#69F0AE");
-        $("button").css("background-color", "#69F0AE");
-        
+        refreshUI(imgUrl[2], "#FF9800", "#FFAB40");
+    } else if(degF >= 50 && degF <= 80){
+        refreshUI(imgUrl[1], "#4CAF50", "#69F0AE");
+    } else if(degF < 50){
+        refreshUI(imgUrl[0], "#3F51B5", "#536DFE");
     } else {
-        $("body").css("background-image", "url(" + imgUrl[0] + ")");
-        $("div").css("background-color", "#3F51B5");
-        $("div.answer").css("background-color", "#536DFE");
-        $("button").css("background-color", "#536DFE");
-    } // fade transition?
+    }
+    
     $("#cel").val("");
 }
 function fahrConvert() {
@@ -36,36 +29,57 @@ function fahrConvert() {
     var degFloat = parseFloat(degF);
     var degC = (degFloat - 32)*(5/9);
     
-    $(".display").html(degFloat.toFixed() + "&#176;F" + " = " + degC.toFixed() + "&#176;C");
+    if(isNaN(degFloat)){
+        $(".display").html("Please enter a number");
+        $("p").addClass("error");
+    } else {
+        $(".display").html(degFloat.toFixed() + "&#176;F" + " = " + degC.toFixed() + "&#176;C");
+        $("p").removeClass("error");
+    }
     
     if(degC >= 48.5) {
-        $("body").css("background-image", "url(" + imgUrl[3] + ")");
-        $("div").css("background-color", "#F44336");
-        $("div.answer").css("background-color", "#FF5252");
-        $("button").css("background-color", "#FF5252");
-        
+        refreshUI(imgUrl[3], "#F44336", "#FF5252");
     } else if(degC >= 26.5 && degC < 48.5){
-        $("body").css("background-image", "url(" + imgUrl[2] + ")");
-        $("div").css("background-color", "#FF9800");
-        $("div.answer").css("background-color", "#FFAB40");
-        $("button").css("background-color", "#FFAB40");
-        
+        refreshUI(imgUrl[2], "#FF9800", "#FFAB40");
     } else if(degC >= 10 && degC < 26.5){
-        $("body").css("background-image", "url(" + imgUrl[1] + ")");
-        $("div").css("background-color", "#4CAF50");
-        $("div.answer").css("background-color", "#69F0AE");
-        $("button").css("background-color", "#69F0AE");
-        
+        refreshUI(imgUrl[1], "#4CAF50", "#69F0AE");
+    } else if(degC < 10){
+        refreshUI(imgUrl[0], "#3F51B5", "#536DFE");
     } else {
-        $("body").css("background-image", "url(" + imgUrl[0] + ")");
-        $("div").css("background-color", "#3F51B5");
-        $("div.answer").css("background-color", "#536DFE");
-        $("button").css("background-color", "#536DFE");
     }
+    
     $("#fahr").val("");
 }
 
 var imgUrl = new Array("images/cold.jpg", "images/warm.jpg", "images/hot.jpg", "images/boiling.jpg");
 
+function refreshUI(imageName, divColor, answerAndButtonColor){
+    $("body").css("background-image", "url(" + imageName + ")");
+    $("div").css("background-color", divColor);
+    $("div.answer").css("background-color", answerAndButtonColor);
+    $("button").css("background-color", answerAndButtonColor);
+}
+
 $("#go1").click(celConvert);
 $("#go2").click(fahrConvert);
+
+var parent, ripple, d, x, y;
+$("button").click(function(e){
+	parent = $(this).parent();
+	if(parent.find(".ripple").length == 0){
+		parent.prepend("<span class='ripple'></span>");
+    }
+    
+	ripple = parent.find(".ripple");
+	ripple.removeClass("animate");
+	
+	if(!ripple.height() && !ripple.width()){
+        d = Math.max(parent.outerWidth(), parent.outerHeight());
+		ripple.css({height: d, width: d});
+	}
+
+	x = e.pageX - parent.offset().left - ripple.width()/2;
+	y = e.pageY - parent.offset().top - ripple.height()/2;
+
+	ripple.css({top: y+'px', left: x+'px'}).addClass("animate");
+})
