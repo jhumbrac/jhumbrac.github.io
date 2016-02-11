97f23349d930718041ce2cfabbc3d532747217d3
jhumbrac <jhumbrac@gmail.com>
2016-02-11 12:46:52 -0600

    updates

---
+++ DRS/home.php
@@ -0,0 +1,154 @@
+<?php
+/*
+*
+* Template Name: Home
+*
+*/
+
+get_header(); ?>
+
+<main role="main">
+    <section id="splash">
+	<div>
+		<div>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/logoWhite.svg" alt="David Randolph Smith and Associates logo">
+		        <h1>Nashville Medical Malpractice Law Firm<br>Tennessee Personal Injury Law Firm<br>Nashville Wrongful Death Attorneys</h1>
+		</div>
+	</div>
+    </section>
+    <section id="yourCase">
+	<h2>It's not about <em>a case</em>, <br>it's about <strong>your case</strong></h2>
+	<p>We understand this sentiment. Our Nashville law firm was built on the belief we fight for a cause – your cause. If we take your case, we will not quit until we reach the best possible verdict or settlement for you.</p>
+	<a href="#">Contact us about your case today</a>
+    </section>
+    <section id="reputation">
+        <h2>Outstanding Reputation</h2>
+        <p>The firm’s lead trial attorney, David Randolph Smith, has been a trial attorney since 1978, a professor of law at Vanderbilt Law School (1983-1988) and is board-certified as a specialist in medical malpractice litigation. The firm has consistently been recognized in U.S. News &amp; World Report’s Best Law Firms Rankings. We have been voted “Super Lawyers (2007-2015)” “Best Lawyers in America,” “150 Best Lawyers in Tennessee” “Mid-South Super Lawyers” and “Top 100 Lawyers in Tennessee” (Super Lawyers List) and “Top 100 Trial Attorneys”</p>
+        <ul class="clearfix">
+            <li>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/superLawyers.png" alt="Super Lawyers logo">
+                <p>Super Lawyers (2007-2014)<br>Mid-South Super Lawyers<br>Top 100 Lawyers in Tennessee</p>
+            </li>
+            <li><img src="<?php echo get_template_directory_uri(); ?>/img/bestLawyers.jpg" alt="Best Lawyers in America logo"></li>
+            <li><img src="<?php echo get_template_directory_uri(); ?>/img/bestLawyersTn.jpg" alt="Business Tennessee Best 150 Lawyers"></li>
+            <li><img src="<?php echo get_template_directory_uri(); ?>/img/Top100seal.png" alt="Top 100 trial lawyers logo"></li>
+        </ul>
+    </section>
+    <section id="fight">
+	<div>
+	        <h2>We will fight for you</h2>
+	        <p>At David Randolph Smith &amp; Associates, we have obtained judgments and settlements in excess of $500 million dollars in personal injury lawsuits.</p>
+	        <p>We don’t just claim to get good results for our clients, we actually do it. Here are results that show the type of representation provided by our experienced and respected legal team:</p>
+	</div>
+        <section id="recoveries">
+            <h3>Remarkable recoveries in personal injury lawsuits</h3>
+            <ul class="clearfix">
+                <li><div><i class="flaticon-judge1"></i><span>$193</span><span>Million</span>Qui Tam Suit</div></li>
+                <li><div><i class="flaticon-law"></i><span>$80</span><span>Million</span>Crematory Cases</div></li>
+                <li><div><i class="flaticon-judge1"></i><span>$39.5</span><span>Million</span>Crematory Litigation</div></li>
+                <li><div><i class="flaticon-law"></i><span>$11</span><span>Million</span>for medical malpractice wrongful death</div></li>
+            </ul>
+            <a href="#" class="cta">More Settlements &amp; Verdicts</a>
+    </section>
+    </section>
+    <div class="clearfix">
+	    <section id="accountable">
+            <div>
+                <h2>The resources to hold the powerful accountable</h2>
+	           <p>We advance the costs in our clients’ cases and, unlike many personal injury firms, we have never charged our clients interest on case expenses and do not use litigation finance companies.</p>
+            </div>
+	    </section>
+	    <section id="noFees">
+            <div>
+                <h2>No Legal Fees Unless We Recover &amp; No Finance Charges</h2>
+                <p>If we accept your case, there is NO charge and no legal fees until we recover on your case. We work on a contingency basis, regardless of the client’s ability to pay. If we do not recover any money for you, you owe us no legal fees.</p>
+            </div>
+	    </section>
+    </div>
+    <section id="dedicated" class="clearfix">
+        <h2>Dedicated Trial Lawyers &amp; Experienced Personal Injury Attorneys</h2>
+        <p>We have decades of experience litigating accident and injury cases in Nashville, Tennessee (and the Mid-South), especially in medical malpractice, products liability, automobile tractor-trailer collision, and car accident cases.</p>
+        <section>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/david600.jpg" alt="David Randolph Smith attorney at law">
+            <div>
+                <h3>David R. Smith</h3>
+                <ul>
+                    <li><a href="mailto:drs@drslawfirm.com"><i class="flaticon-email5"></i>drs@drslawfirm.com</a></li>
+                    <li><a href="#"><i class="flaticon-phone21"></i></a></li>
+                    <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                    <li><a href="#">See Profile</a></li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/dom600.jpg" alt="Dominick R. Smith attorney at law">
+            <div>
+                <h3>Dominick R. Smith</h3>
+                <ul>
+                    <li><a href="mailto:dom@drslawfirm.com"><i class="flaticon-email5"></i>dom@drslawfirm.com</a></li>
+                    <li><a href="#"><i class="flaticon-phone21"></i></a></li>
+                    <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                    <li><a href="#">See Profile</a></li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/lyon600.jpg" alt="W. Lyon Chadwick attorney at law">
+            <div>
+                <h3>Lyon Chadwick, Jr.</h3>
+                <ul>
+                    <li><a href="mailto:LYON@drslawfirm.com"><i class="flaticon-email5"></i>LYON@drslawf	irm.com</a></li>
+                    <li><a href="#"><i class="flaticon-phone21"></i></a></li>
+                    <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                    <li><a href="#">See Profile</a></li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chris600.jpg" alt="Chris Smith attorney at law">
+            <div>
+                <h3>Chris Smith</h3>
+                <ul>
+                    <li><a href="mailto:"><i class="flaticon-email5"></i></a></li>
+                    <li><a href="#"><i class="flaticon-phone21"></i></a></li>
+                    <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                    <li><a href="#">See Profile</a></li>
+                </ul>
+            </div>
+        </section>
+    </section>
+    <section id="scope">
+        <div>
+            <h2>Our Practice</h2>
+            <ul>
+                <li><a href="/tn-personal-injury/"><i class="flaticon-accident1"></i>Personal Injury</a></li>
+                <li><a href="/police-brutality-cases/"><i class="flaticon-law3"></i>Police Brutality</a></li>
+                <li><a href="/wrongful-death/"><i class="flaticon-grave"></i>Wrongful Death</a></li>
+                <li><a href="/tn-truck-safety-lawyers/"><i class="flaticon-delivery55"></i>Truck Accidents</a></li>
+                <li><a href="/automobile-accidents-faq/"><i class="flaticon-collision"></i>Automobile Accidents</a></li>
+                <li><a href="/nashville-tn-products-liability-attorney/"><i class="flaticon-law"></i>Product Liability</a></li>
+                <li><a href="/nashville-bike-accident-lawyer/"><i class="flaticon-transport674"></i>Bike Accidents</a></li>
+                <li><a href="/tennessee-civil-rights-attorneys/"><i class="flaticon-agreement"></i>Civil Rights</a></li>
+                <li><a href="/tn-medical-malpractice-attorney/"><i class="flaticon-hospital25"></i>Medical Malpractice</a></li>
+                <li><a href="/tennessee-legal-malpractice-attorneys/"><i class="flaticon-law"></i>Legal Malpractice</a></li>
+                <li><a href="/commercial-litigation/"><i class="flaticon-coins15"></i>Commercial Litigation</a></li>
+            </ul>
+        </div>
+    </section>
+    <section id="exceptional" class="clearfix">
+        <div>
+		<h2>It's not about doing a job - it's about being exceptional</h2>
+		<h3>There are approximately 17,000 attorneys practicing law in Tennessee, so how do you pick the right one?</h3>
+		<p>While many of these attorneys and law firms may be good, there are only 10 plaintiffs’ attorneys in the entire state that are board-certified medical malpractice specialists. Very few law firms regularly try jury cases, and many “prominent” Nashville and Tennessee personal injury lawyers and law firms do not write their own briefs and “farm out” legal research and appeals. Even fewer firms have won landmark decisions or taught personal injury law (torts, evidence and ethics) as a law professor. We are fortunate to have been recognized by our peers on “best lawyers” surveys and lists and have engaged in tireless efforts to support positive changes in the law with numerous important landmark tort cases won in the Tennessee and Kentucky Supreme Courts.</p>
+            <p class="strong">That’s who we are.</p>
+        </div>
+        <aside id="advertises">
+		<div>
+	 		<h2>Should I hire a Lawyer Who Advertises on TV?</h2>
+        		<p>There is something very important to remember, when it comes to hiring a personal injury attorney — the best personal injury attorneys often do little or no advertising because they get their cases through “referrals” from other attorneys, due to their reputations for doing excellent work and obtaining significant results.</p>
+		</div>
+        </aside>
+    </section>
+</main>
+
+<?php get_footer(); ?>
