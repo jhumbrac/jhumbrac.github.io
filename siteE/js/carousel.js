dd40c69f237489db93c7f871b1aa45d288fb2a14
John Humbracht <jhumbrac@gmail.com>
2022-08-07 23:19:15 -0500

    Removed inertia

--- siteE/js/carousel.js
+++ siteE/js/carousel.js
@@ -57,8 +57,8 @@ function initCarousel(carousel) {
     trigger: carousel,
     onDrag: updateProgress,
     allowContextMenu: true,
-    // Premium
-    inertia: true,
+    // // Premium
+    // inertia: true,
     onThrowUpdate: updateProgress,    
     snap: options    
   });
