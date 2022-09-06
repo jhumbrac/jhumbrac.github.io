27b0f6580c10dd2f9917ab8cbc5b5b7034bc05ca
John Humbracht <jhumbrac@gmail.com>
2022-09-05 20:55:08 -0500

    site update

--- site-test/js/timeline.js
+++ site-test/js/timeline.js
@@ -4,7 +4,19 @@ let tl = gsap.timeline({
       start: "top top", // when the top of the trigger hits the top of the viewport
       end: "+=200", // end after scrolling 500px beyond the start
       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    },
+    scrollTrigger: {
+        trigger: ".underline",
+        start: "bottom bottom",
+        scrub: 1,
     }
   });
-let logo = document.querySelector('.logo')
+let logo = document.querySelector('.logo');
+let underline = document.querySelectorAll('.underline:after');
 tl.to(logo, {width: "10em"})
+tl.from(underline, {
+    translateX: 32,
+    onUpdate: function() {
+        console.log('trigger is working');
+    }
+})
